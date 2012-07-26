d036cc346bbb5551a473b6c71b0ad341585b772a
Onur Baykal <theaob@gmail.com>
2012-07-26 15:49:39 +0300

    sizeHint() added

--- button.cpp
+++ button.cpp
@@ -5,3 +5,12 @@ Button::Button(const QString &text, QWidget *parent)
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
     setText(text);
 }
+
+QSize Button::sizeHint() const
+{
+    QSize size = QToolButton::sizeHint();
+    size.rheight() += 20;
+    size.rwidth() = qMax(size.width(), size.height());
+
+    return size;
+}
